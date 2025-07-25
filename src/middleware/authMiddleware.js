bfb720ddb02869ad28e9db9bf4051362bf1030fc
fauzihazim <fauzihazim28@gmail.com>
2025-07-25 15:42:47 +0700

    add: updateUser

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const validateEmail = [
   body('email')
@@ -53,3 +55,26 @@ export const registerValidator = [
   handleValidationErrors
 ];
 
+export const authenticateAccessToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split('Bearer ')[1];
+  if (!token) {
+    return res.status(401).json({
+      status: 108,
+      message: "Token tidak tidak valid atau kadaluwarsa",
+      data: null
+    });
+  };
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(401).json({
+        status: 108,
+        message: "Token tidak tidak valid atau kadaluwarsa",
+        data: null
+      });
+    }
+    res.locals.userId = user.sub;
+    res.locals.email = user.email;
+    next();
+  });
+};
