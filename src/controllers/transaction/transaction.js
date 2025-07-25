4f7fa07a032fb3e482c6e55904dcc2370c358c87
fauzihazim <fauzihazim28@gmail.com>
2025-07-25 21:07:08 +0700

    add: topUp

--- src/controllers/transaction/transaction.js
+++ src/controllers/transaction/transaction.js
@@ -1,4 +1,3 @@
-// import 'dotenv/config';
 import { conn } from '../../utils/db.js';
 
 export const getBalance = async (req, res) => {
@@ -8,13 +7,42 @@ export const getBalance = async (req, res) => {
             'SELECT balance FROM users WHERE email = ?',
             [email]
         );
-        console.log(userBalance.balance);
-        
         res.status(200).json({
             status: 0,
             message: "Get Balance Berhasil",
             data: {
                 balance: userBalance.balance
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            error: "Internal server error"
+        });
+    }
+}
+
+export const topUp = async (req, res) => {
+    const {top_up_amount} = req.body;
+    try {
+        const email = res.locals.email;
+        const [addBalance] = await conn.execute(
+            'UPDATE users SET balance = balance + ? WHERE email = ?',
+            [top_up_amount, email]
+        );
+        if (!addBalance.affectedRows) {
+            throw new Error("Failed top up");
+        }
+        const [[balance]] = await conn.execute(
+            'SELECT balance FROM users WHERE email = ?',
+            [email]
+        );        
+        
+        res.status(200).json({
+            status: 0,
+            message: "Top Up Balance berhasil",
+            data: {
+                balance
             }
         });
     } catch (error) {
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authenticateAccessToken } from '../middleware/authMiddleware.js';
-import { getBalance } from '../controllers/transaction/transaction.js';
+import { getBalance, topUp } from '../controllers/transaction/transaction.js';
+import { topUpValidator } from '../middleware/transactionMiddleware.js';
 
 const app = express();
 app.use(express.json()); 
 
 app.get('/balance', authenticateAccessToken, getBalance);
+app.post('/topup', authenticateAccessToken, topUpValidator, topUp);
 export default app;
