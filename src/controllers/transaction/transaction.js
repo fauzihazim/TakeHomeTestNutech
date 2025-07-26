5ea93d4d4d0e21a2ab8a78675343aefecbb81e78
fauzihazim <fauzihazim28@gmail.com>
2025-07-26 13:58:29 +0700

    fix topUp

--- src/controllers/transaction/transaction.js
+++ src/controllers/transaction/transaction.js
@@ -33,25 +33,29 @@ export const getBalance = async (req, res) => {
 export const topUp = async (req, res) => {
     const {top_up_amount} = req.body;
     let connection;
-    let transactionResult;
+    const email = res.locals.email;
+    const userId = res.locals.userId;
     try {
-        const email = res.locals.email;
-        const userId = res.locals.userId;
-        [transactionResult] = await conn.execute(
-            'INSERT INTO transactions (idUser, amount, transaction_type) VALUES (?, ?, ?)',
-            [userId, top_up_amount, 'TOPUP']
-        );
+        
         connection = await conn.getConnection();
         await connection.beginTransaction();
         await connection.execute(
             'UPDATE users SET balance = balance + ? WHERE email = ? AND idUser = ?',
             [top_up_amount, email, userId]
         );
+        const [transactionResult] = await connection.execute(
+            'INSERT INTO transactions (idUser, amount, transaction_type) VALUES (?, ?, ?)',
+            [userId, top_up_amount, 'TOPUP']
+        );
+        console.log(transactionResult.insertId);
+        
         const invoiceNumber = generateInvoice(transactionResult.insertId);
+        console.log("here 1");
         await connection.execute(
-            'INSERT INTO topup (idTransaction, amount, topupStatus, invoice_numer) VALUES (?, ?, ?, ?)',
+            'INSERT INTO topup (idTransaction, amount, topupStatus, invoice_number) VALUES (?, ?, ?, ?)',
             [transactionResult.insertId, top_up_amount, 'SUCCESS', invoiceNumber]
         );
+        console.log("here 1");
         const [[userBalance]] = await connection.execute(
             'SELECT balance FROM users WHERE email = ?',
             [email]
@@ -65,9 +69,82 @@ export const topUp = async (req, res) => {
             }
         });
     } catch (error) {
+        console.error(error);
+        
         try {
             if (connection) {
                 await connection.rollback();
+                const [transactionResult] = await conn.execute(
+                    'INSERT INTO transactions (idUser, amount, transaction_type) VALUES (?, ?, ?)',
+                    [userId, top_up_amount, 'TOPUP']
+                );
+                if (transactionResult?.insertId) {
+                    await conn.execute(
+                        'INSERT INTO topup (idTransaction, amount, topupStatus) VALUES (?, ?, ?)',
+                        [transactionResult.insertId, top_up_amount, 'FAILED']
+                    );
+                }
+            }
+        } catch (rollbackError) {
+            console.error('Rollback error:', rollbackError);
+        }
+        res.status(500).json({
+            status: "failed",
+            error: "Internal server error"
+        });
+    } finally {
+        if (connection) connection.release();
+    }
+}
+
+export const payment = async (req, res) => {
+    const {top_up_amount} = req.body;
+    let connection;
+    const email = res.locals.email;
+    const userId = res.locals.userId;
+    try {
+        
+        connection = await conn.getConnection();
+        await connection.beginTransaction();
+        await connection.execute(
+            'UPDATE users SET balance = balance + ? WHERE email = ? AND idUser = ?',
+            [top_up_amount, email, userId]
+        );
+        const [transactionResult] = await connection.execute(
+            'INSERT INTO transactions (idUser, amount, transaction_type) VALUES (?, ?, ?)',
+            [userId, top_up_amount, 'TOPUP']
+        );
+        console.log(transactionResult.insertId);
+        
+        const invoiceNumber = generateInvoice(transactionResult.insertId);
+        console.log("here 1");
+        await connection.execute(
+            'INSERT INTO topup (idTransaction, amount, topupStatus, invoice_number) VALUES (?, ?, ?, ?)',
+            [transactionResult.insertId, top_up_amount, 'SUCCESS', invoiceNumber]
+        );
+        console.log("here 1");
+        const [[userBalance]] = await connection.execute(
+            'SELECT balance FROM users WHERE email = ?',
+            [email]
+        );
+        await connection.commit();
+        res.status(200).json({
+            status: 0,
+            message: "Get Balance Berhasil",
+            data: {
+                balance: userBalance.balance
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        
+        try {
+            if (connection) {
+                await connection.rollback();
+                const [transactionResult] = await conn.execute(
+                    'INSERT INTO transactions (idUser, amount, transaction_type) VALUES (?, ?, ?)',
+                    [userId, top_up_amount, 'TOPUP']
+                );
                 if (transactionResult?.insertId) {
                     await conn.execute(
                         'INSERT INTO topup (idTransaction, amount, topupStatus) VALUES (?, ?, ?)',
